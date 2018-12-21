2baf4afd6f012c606cb9818efa949f5eee321a8b
Christian Seiler <christian@christianseiler.ch>
2018-12-21 09:51:56 +0100

    add pdf with code

--- Remote.cpp
+++ Remote.cpp
@@ -1,24 +1,24 @@
 // Return values of the IR Remote
 enum Remote : unsigned long {
-  //  CHNEG =   0xFFA25D,
-  //  CH =      0xFF629D,
-  //  CHPLUS =  0xFFE21D,
+    CHNEG =   0xFFA25D,
+    CH =      0xFF629D,
+    CHPLUS =  0xFFE21D,
   LEFT =    0xFF22DD,
   RIGHT =   0xFF02FD,
   PLAY =    0xFFC23D,
-  //  MINUS =   0xFFE01F,
-  //  PLUS =    0xFFA857,
-  //  EQ =      0xFF906F
-  //  ONEPLUS = 0xFF9867,
-  //  TWOPLUS = 0xFFB04F,
-  //  ZERO =    0XFF6897,
+    MINUS =   0xFFE01F,
+    PLUS =    0xFFA857,
+    EQ =      0xFF906F
+    ONEPLUS = 0xFF9867,
+    TWOPLUS = 0xFFB04F,
+    ZERO =    0XFF6897,
   ONE =     0xFF30CF,
   TWO =     0xFF18E7,
-  //  THREE =   0xFF7A85,
-  //  FOUR =    0xFF10EF,
-  //  FIVE =    0xFF38C7,
-  //  SIX =     0xFF5AA5,
-  //  SEVEN =   0xFF42BD,
-  //  EIGHT =   0xFF4AB5,
-  //  NINE =    0xFF52AD
+    THREE =   0xFF7A85,
+    FOUR =    0xFF10EF,
+    FIVE =    0xFF38C7,
+    SIX =     0xFF5AA5,
+    SEVEN =   0xFF42BD,
+    EIGHT =   0xFF4AB5,
+    NINE =    0xFF52AD
 };
